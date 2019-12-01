9f2a02827093e34c47b168ab3f3b05b9a45e9cb6
chikechris <cchike2017@gmail.com>
2019-12-01 05:27:09 -0500

    connected to mongo DB, tested registered route

---
+++ config/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const url = process.env.URL
+
+const connectDB = () => {
+  mongoose
+    .connect(
+      url,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => {
+      console.error(err.message)
+      process.exit(1)
+    })
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+const connectDB = require('./config/db')
+
+// connects to the database
+connectDB()
+
+app.use(express.json({ extended: true }))
+
+app.use('/register', require('./routes/register'))
 
 
 const PORT = process.env.PORT || 5000
