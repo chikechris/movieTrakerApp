29a37f5f590b65eb6168777f1c1bd3af3ce4f30b
chikechris <cchike2017@gmail.com>
2019-12-01 05:37:09 -0500

    working on register.js file

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
 const router = require('express').Router()
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 
 router.post('/', (req,res)=>{
