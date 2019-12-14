4912ba8c2f0abc6034481c50dfcc52259a2572c5
chikechris <cchike2017@gmail.com>
2019-12-14 06:12:59 -0500

    changed the port back to 5000

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ if(process.env.NODE_ENV === 'production') {
 
 
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
